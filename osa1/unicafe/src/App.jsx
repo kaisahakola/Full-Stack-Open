a06b7e99cc69f12a6d2e2dfcafe59ed693726b7c
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-25 14:00:05 +0200

    Osa 1, tehtävä 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,22 +6,71 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
+const Statistic = ({ text, statistic }) => {
+  if (text === 'positive') {
+    return (
+      <>
+        <p>{text} {statistic} %</p>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <p>{text} {statistic}</p>
+      </>
+    )
+  }
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avarage, setAvarage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodButton = () => {
-    setGood(good + 1)
+    const newGood = good + 1
+    const newAll = all + 1
+    setGood(newGood)
+    setAll(newAll)
+    handleAvarage(newGood, bad, newAll)
+    handlePositive(newGood, newAll)
   }
 
   const handleNeutralButton = () => {
-    setNeutral(neutral + 1)
+    const newNeutral = neutral + 1
+    const newAll = all + 1
+    setNeutral(newNeutral)
+    setAll(newAll)
+    handleAvarage(good, bad, newAll)
+    handlePositive(good, newAll)
   }
 
   const handleBadButton = () => {
-    setBad(bad + 1)
+    const newBad = bad + 1
+    const newAll = all + 1
+    setBad(newBad)
+    setAll(newAll)
+    handleAvarage(good, newBad, newAll)
+    handlePositive(good, newAll)
+  }
+
+  const handleAvarage = (newGood, newBad, newAll) => {
+    console.log("good: ", newGood)
+    console.log("bad: ", newBad)
+    console.log("all: ", newAll)
+
+    const value = newGood * 1 + newBad * -1
+    // Using ternary operator to check if all is 0
+    setAvarage(all === 0 ? 0 : value / newAll)
+  }
+
+  const handlePositive = (newGood, newAll) => {
+    // Using ternary operator to check if all is 0
+    setPositive(newAll === 0 ? 0 : (newGood / newAll) * 100)
   }
 
   return (
@@ -30,11 +79,13 @@ const App = () => {
       <Button handleClick={handleGoodButton} text='good' />
       <Button handleClick={handleNeutralButton} text='neutral' />
       <Button handleClick={handleBadButton} text='bad' />
-      <br />
       <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistic text='good' statistic={good} />
+      <Statistic text='neutral' statistic={neutral} />
+      <Statistic text='bad' statistic={bad} />
+      <Statistic text='all' statistic={all} />
+      <Statistic text='avarage' statistic={avarage} />
+      <Statistic text='positive' statistic={positive} />
     </div>
   )
 }
