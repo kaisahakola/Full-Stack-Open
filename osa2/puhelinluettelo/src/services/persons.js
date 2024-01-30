d8279e4e16a8446ec7c58f07565346738c031bc0
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-30 11:19:44 +0200

    Osa 2, tehtävät 2.6. - 2.17.

---
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3002/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const addPerson = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const deletePerson = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const updatePerson = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+export default { getAll, addPerson, deletePerson, updatePerson }
