1d5fa605b6b5f725a314b7eedb0e2bc4cc66d090
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-02-03 13:38:38 +0200

    uibuild

--- osa3/puhelinluettelon_be/index.js
+++ osa3/puhelinluettelon_be/index.js
@@ -3,8 +3,9 @@ const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('data', function (req, res) {
     return JSON.stringify(req.body)
