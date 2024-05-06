c24c9ac93616f76d7f775f3347614d7735f8ebe0
Kaisa Hakola <kaisahakola@Kaisa-MacBook-Pro.local>
2024-05-06 14:52:51 +0300

    Korjauksia 7 osaa varten

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,42 +1,48 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const path = require("path");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-logger.info('connecting to', config.MONGO_URI)
+logger.info("connecting to", config.MONGO_URI);
 
-mongoose.connect(config.MONGO_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', middleware.userExtractor, blogRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/blogs", middleware.userExtractor, blogRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
-if (process.env.NODE_ENV === 'test') {
-    const testingRouter = require('./controllers/testing')
-    app.use('/api/testing', testingRouter)
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
 }
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
-module.exports = app
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,83 +1,85 @@
-const blogRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const { userExtractor } = require('../utils/middleware')
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { userExtractor } = require("../utils/middleware");
 
-blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('user', {
-        username: 1,
-        name: 1
-    })
-    response.json(blogs)
-})
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
+  response.json(blogs);
+});
 
-blogRouter.post('/', userExtractor, async (request, response) => {
-    const body = request.body
+blogRouter.post("/", userExtractor, async (request, response) => {
+  const body = request.body;
 
-    let likes = 0
-    if (body.likes) {
-        likes += body.likes
-    }
+  let likes = 0;
+  if (body.likes) {
+    likes += body.likes;
+  }
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
 
-    const user = request.user
+  const user = request.user;
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: likes,
-        user: user._id
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: likes,
+    user: user._id,
+  });
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
-    if (!body.title || !body.url) {
-        response.status(400).end()
-    } else {
-        response.status(201).json(savedBlog)
-    }
-})
+  if (!body.title || !body.url) {
+    response.status(400).end();
+  } else {
+    response.status(201).json(savedBlog);
+  }
+});
 
-blogRouter.delete('/:id', userExtractor, async (request, response) => {
+blogRouter.delete("/:id", userExtractor, async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
 
-    const blog = await Blog.findById(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
+  const user = request.user;
 
-    const user = request.user
+  if (blog.user && blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } else {
+    return response
+      .status(403)
+      .json({ error: "You are not authorized to delete this blog" });
+  }
+});
 
-    if (blog.user && blog.user.toString() === user.id.toString()) {
-        await Blog.findByIdAndDelete(request.params.id)
-        response.status(204).end()
-    } else {
-        return response.status(403).json({ error: 'You are not authorized to delete this blog' });
-    }
-    
-})
-  
-blogRouter.put('/:id', async (request, response) => {
-    const body = request.body
+blogRouter.put("/:id", async (request, response) => {
+  const body = request.body;
 
-    const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    }
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    response.json(updatedBlog)
-})
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
 
-module.exports = blogRouter
+module.exports = blogRouter;
